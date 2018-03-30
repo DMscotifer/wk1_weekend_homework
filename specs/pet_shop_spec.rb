afa9b151d4f921781233dd8053247128038bf555
Scott Stevenson <scotifer92@googlemail.com>
2018-03-30 13:19:08 +0100

    7 working tests

--- specs/pet_shop_spec.rb
+++ specs/pet_shop_spec.rb
@@ -99,16 +99,16 @@ class TestPetShop < Minitest::Test
     assert_equal(0, sold)
   end
 
-  # def test_increase_pets_sold
-  #   increase_pets_sold(@pet_shop,2)
-  #   sold = pets_sold(@pet_shop)
-  #   assert_equal(2, sold)
-  # end
+  def test_increase_pets_sold
+    increase_pets_sold(@pet_shop,2)
+    sold = pets_sold(@pet_shop)
+    assert_equal(2, sold)
+  end
 
-  # def test_stock_count
-  #   count = stock_count(@pet_shop)
-  #   assert_equal(6, count)
-  # end
+  def test_stock_count
+    count = stock_count(@pet_shop)
+    assert_equal(6, count)
+  end
 
   # def test_all_pets_by_breed__found
   #   pets = pets_by_breed(@pet_shop, "British Shorthair")
