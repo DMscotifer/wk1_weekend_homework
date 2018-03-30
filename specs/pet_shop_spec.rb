ebf54fbcfab5b5e07b958fc486acad938590b7ac
Scott Stevenson <scotifer92@googlemail.com>
2018-03-30 14:04:15 +0100

    pets_by_breed

--- specs/pet_shop_spec.rb
+++ specs/pet_shop_spec.rb
@@ -110,15 +110,15 @@ class TestPetShop < Minitest::Test
     assert_equal(6, count)
   end
 
-  # def test_all_pets_by_breed__found
-  #   pets = pets_by_breed(@pet_shop, "British Shorthair")
-  #   assert_equal(2, pets.count)
-  # end
+  def test_all_pets_by_breed__found
+    pets = pets_by_breed(@pet_shop, "British Shorthair")
+    assert_equal(2, pets.count)
+  end
 
-  # def test_all_pets_by_breed__not_found
-  #   pets = pets_by_breed(@pet_shop, "Dalmation")
-  #   assert_equal(0, pets.count)
-  # end
+  def test_all_pets_by_breed__not_found
+    pets = pets_by_breed(@pet_shop, "Dalmation")
+    assert_equal(0, pets.count)
+  end
 
   # def test_find_pet_by_name__returns_pet
   #   pet = find_pet_by_name(@pet_shop, "Arthur")
